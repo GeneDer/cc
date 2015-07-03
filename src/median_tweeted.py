5d1896f0e479888dfca4377c3486ec781b682adb
Gene Der Su <e870252314@yahoo.com.tw>
2015-07-02 20:57:08 -0700

    Add some comment to explain the code.

--- src/median_tweeted.py
+++ src/median_tweeted.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # example of program that calculates the median number of unique words per tweet.
-# written by Gene Der Su for Insight coding challenge.
+# Written by Gene Der Su for Insight Coding Challenge.
 
 import sys
-import math
 
 
+## read data file
 inputFile=sys.argv[1]
 with open(inputFile,'r') as i:
     lines = i.readlines()
@@ -15,6 +15,7 @@ uniqueWordCountArray=[]
 medianArray=[]
 medianIndex=0
 for line in lines:
+    ## create sorted unique word count array
     words = line.split()
     uniqueWordCount=len(list(set(words)))
     uniqueWordCountArray.append(uniqueWordCount)
@@ -22,7 +23,8 @@ for line in lines:
         if wordCount>uniqueWordCount:
             uniqueWordCountArray.insert(index,uniqueWordCount)
             uniqueWordCount.pop(len(uniqueWordCount)-1)
-    
+
+    ## finding median and attach them to the median array
     if len(uniqueWordCountArray)%2==0:
         median=0.5*(uniqueWordCountArray[medianIndex]+uniqueWordCountArray[medianIndex+1])
     elif len(uniqueWordCountArray)!=1:
@@ -31,7 +33,8 @@ for line in lines:
     else:
         median=uniqueWordCount
     medianArray.append(median)
-        
+
+## write the result into the output file
 outputFile=sys.argv[2]
 with open(outputFile,'w') as o:
     for median in sorted(medianArray):
--- src/words_tweeted.py
+++ src/words_tweeted.py
@@ -6,31 +6,35 @@ import sys
 import math
 
 
+## read data file
 inputFile=sys.argv[1]
 with open(inputFile,'r') as i:
     lines = i.readlines()
 
 maxLetter=0
-maxNumber=1
+maxCount=1
 wordCount={}
 for line in lines:
+    ## count number of times each word appears while keeping the number of maximum letter and maximum count
     words = line.split()
     for word in words:
         if maxLetter<len(word):
             maxLetter=len(word)
         if word in wordCount:
             wordCount[word]=wordCount[word]+1
-            if maxNumber<wordCount[word]:
-                maxNumber=wordCount[word]
+            if maxCount<wordCount[word]:
+                maxCount=wordCount[word]
         else:
             wordCount[word]=1
 
-maxNumberDigit=int(math.ceil((math.log10(maxNumber))))
+## calculate the digits needed for the number of count
+maxCountDigit=int(math.ceil((math.log10(maxCount))))
 
+## write the result into the output file
 outputFile=sys.argv[2]
 with open(outputFile,'w') as o:
     for word in sorted(wordCount):
-        resultingString = '{0:%s}      {1:%sd}'%(maxLetter,maxNumberDigit)
+        resultingString = '{0:%s}      {1:%sd}'%(maxLetter,maxCountDigit)
         o.write(resultingString.format(word, wordCount[word]))
         o.write('\n')
 
